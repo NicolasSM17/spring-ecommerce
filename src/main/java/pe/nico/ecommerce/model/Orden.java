13aef23d18321d0d5895023c33c3602c4a7e6587
NicolasSM17 <segoviamatosjesunicolas@gmail.com>
2023-07-02 19:57:46 -0500

    crear clases model, mapeo clases a tablas con JPA, integracion del diseño y las vistas

--- src/main/java/pe/nico/ecommerce/model/Orden.java
+++ src/main/java/pe/nico/ecommerce/model/Orden.java
@@ -2,6 +2,13 @@ package pe.nico.ecommerce.model;
 
 import java.util.Date;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,10 +16,20 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "ordenes")
 public class Orden {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String numero;
 	private Date fechaCreacion;
 	private Date fechaRecibida;
 	private double total;
+	
+	@ManyToOne
+	private Usuario usuario;
+	
+	@OneToOne(mappedBy = "orden")
+	private DetalleOrden detalle;
 }
--- src/main/java/pe/nico/ecommerce/model/Producto.java
+++ src/main/java/pe/nico/ecommerce/model/Producto.java
@@ -1,5 +1,11 @@
 package pe.nico.ecommerce.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,11 +13,18 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "productos")
 public class Producto {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nombre;
 	private String descripcion;
 	private String imagen;
 	private double precio;
 	private int cantidad;
+	
+	@ManyToOne
+	private Usuario usuario;
 }
