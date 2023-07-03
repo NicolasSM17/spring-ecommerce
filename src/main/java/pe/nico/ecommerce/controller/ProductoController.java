fc3731a1b60baa14e7ba8e568c5649bbc3637239
NicolasSM17 <segoviamatosjesunicolas@gmail.com>
2023-07-02 20:21:58 -0500

    crear repository y las clases de servicio y el metodo show de ProductoController

---
+++ src/main/java/pe/nico/ecommerce/controller/ProductoController.java
@@ -0,0 +1,13 @@
+package pe.nico.ecommerce.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/productos")
+public class ProductoController {
+	
+	public String show() {
+		return "";
+	}
+}
---
+++ src/main/java/pe/nico/ecommerce/service/ProductoService.java
@@ -0,0 +1,12 @@
+package pe.nico.ecommerce.service;
+
+import java.util.Optional;
+
+import pe.nico.ecommerce.model.Producto;
+
+public interface ProductoService {
+	public Producto save(Producto producto);
+	public Optional<Producto> get(Integer id);
+	public void update(Producto producto);
+	public void delete(Integer id);
+}
