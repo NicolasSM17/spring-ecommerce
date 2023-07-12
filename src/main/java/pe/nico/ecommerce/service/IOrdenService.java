4cefb215114ddc0656c9f261bdadafbf43f7a0df
NicolasSM17 <segoviamatosjesunicolas@gmail.com>
2023-07-11 20:50:21 -0500

    metodo para generar el numero de la orden

--- src/main/java/pe/nico/ecommerce/service/IOrdenService.java
+++ src/main/java/pe/nico/ecommerce/service/IOrdenService.java
@@ -1,7 +1,10 @@
 package pe.nico.ecommerce.service;
 
+import java.util.List;
+
 import pe.nico.ecommerce.model.Orden;
 
 public interface IOrdenService {
+	List<Orden> findAll();
 	Orden save(Orden orden);
 }
