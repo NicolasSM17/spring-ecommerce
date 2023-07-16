60093f160386bcdad93d6b30b6349288eb909592
NicolasSM17 <segoviamatosjesunicolas@gmail.com>
2023-07-15 19:57:39 -0500

    implementar template diferente para usuario logeado

--- src/main/java/pe/nico/ecommerce/controller/HomeController.java
+++ src/main/java/pe/nico/ecommerce/controller/HomeController.java
@@ -59,6 +59,9 @@ public class HomeController {
 		
 		model.addAttribute("productos", productoService.findAll());
 		
+		//session 
+		model.addAttribute("sesion", session.getAttribute("idusuario"));
+		
 		return "usuario/home";
 	}
 	
@@ -136,9 +139,12 @@ public class HomeController {
 	}
 	
 	@GetMapping("/getCart")
-	public String getCart(Model model) {
+	public String getCart(Model model, HttpSession session) {
 		model.addAttribute("cart", detalles);
 		model.addAttribute("orden", orden);
+		
+		//session
+		model.addAttribute("sesion", session.getAttribute("idusuario"));
 		
 		return "/usuario/carrito";
 	}
