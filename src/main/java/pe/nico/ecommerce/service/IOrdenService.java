3c00ce4da2f3777a8e5168ca8071496c866a8142
NicolasSM17 <segoviamatosjesunicolas@gmail.com>
2023-07-19 20:46:27 -0500

    funcionalidad ver detalle de compra al user

--- src/main/java/pe/nico/ecommerce/service/IOrdenService.java
+++ src/main/java/pe/nico/ecommerce/service/IOrdenService.java
@@ -1,12 +1,14 @@
 package pe.nico.ecommerce.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import pe.nico.ecommerce.model.Orden;
 import pe.nico.ecommerce.model.Usuario;
 
 public interface IOrdenService {
 	List<Orden> findAll();
+	Optional<Orden> findById(Integer id);
 	Orden save(Orden orden);
 	String generarNumeroOrden();
 	List<Orden> findByUsuario(Usuario usuario);
--- src/main/java/pe/nico/ecommerce/service/OrdenServiceImpl.java
+++ src/main/java/pe/nico/ecommerce/service/OrdenServiceImpl.java
@@ -2,6 +2,7 @@ package pe.nico.ecommerce.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -63,5 +64,11 @@ public class OrdenServiceImpl implements IOrdenService{
 	public List<Orden> findByUsuario(Usuario usuario) {
 		
 		return ordenRepository.findByUsuario(usuario);
+	}
+
+	@Override
+	public Optional<Orden> findById(Integer id) {
+		
+		return ordenRepository.findById(id);
 	}
 }
